4d4666d2b0ab27b5cbb7396a66ec6882bcce2b68
Youssef Amous <youssefamous11@gmail.com>
2024-05-14 11:57:23 +0100

    added search filtering

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  InputGroup,
+  FloatingLabel,
+} from "react-bootstrap";
 import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   const [articles, setArticles] = useState([]);
+  const apiString = "https://fakestoreapi.com/products";
+  // const categoryFilter = "/category";
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=25")
+    fetch(apiString)
       .then((res) => res.json())
       .then((json) => {
         setArticles(json);
@@ -14,9 +23,30 @@ export function Store() {
       });
   }, []);
 
+  const handleInput = (event) => {
+    let input = event.target.value.replace(/\s+/g, " ").trim();
+
+    if (!input) {
+      const savedArticles = localStorage.getItem("articles");
+      if (savedArticles) {
+        setArticles(JSON.parse(savedArticles));
+      }
+    } else {
+      const filteredArticles = articles.filter((e) =>
+        e.title.toLowerCase().includes(input.toLowerCase())
+      );
+
+      setArticles(filteredArticles);
+    }
+  };
+
   return (
     <>
       <h1>Store</h1>
+      <FloatingLabel controlId="searchInput" label="Search" className="mb-3">
+        <Form.Control type="text" placeholder="Search" onChange={handleInput} />
+      </FloatingLabel>
+
       <Row md={2} xs={1} lg={3} className="g-3">
         {articles.map((item) => (
           <Col key={item.id}>
@@ -27,3 +57,4 @@ export function Store() {
     </>
   );
 }
+// search bar (real time), filter, insert. stepper
